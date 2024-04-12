cc82f093f41501f41bc0e57963732895e67dff36
asmckeithan <annesha.mckeithan@gmail.com>
2024-04-12 19:40:01 -0400

    Predicted number of plant growth

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,16 @@
 console.log(`Growth Control System` )
+
+
+const PI = 3.1415;
+let radius = .8;
+const area = PI * radius * radius;
+
+console.log(`Predicted number of plant growth in weeks 1, 2, 3`)
+
+console.log(area) //2.0156 is the area between each plant 
+
+
+for ( let ppw = 1; ppw <= 4; ppw++) {
+      console.log(ppw * 20)
+    }
+    console.log(`Starting at the beginning of week 1 we, start off with 20`)
