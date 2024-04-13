23dbccb76793864d98dae4ac17a8b6c3f9fed8a8
asmckeithan <annesha.mckeithan@gmail.com>
2024-04-12 22:24:00 -0400

    Created switch for weekly crop growth

--- Script/script.js
+++ Script/script.js
@@ -2,15 +2,32 @@ console.log(`Growth Control System` )
 
 
 const PI = 3.1415;
-let radius = .8;
+let radius = 5;
 const area = PI * radius * radius;
+console.log(`calcualtion of filled garden `)
+console.log(area) //2.0156 is the area between each plant
 
-console.log(`Predicted number of plant growth in weeks 1, 2, 3`)
+console.log(`=========================`)
+console.log(`Total plants that meet garden capcity `)
+const totalPlants = area/.8
+console.log(totalPlants )
+ 
 
-console.log(area) //2.0156 is the area between each plant 
 
 
-for ( let ppw = 1; ppw <= 4; ppw++) {
-      console.log(ppw * 20)
-    }
-    console.log(`Starting at the beginning of week 1 we, start off with 20`)
+let plants = 20;
+let week = "1";
+switch (week) {
+   case "1":
+       console.log(`Week 1:${plants * 2}`);
+       break;
+   case "2":
+       console.log(`Week 2:${plants * 4}`);
+       break;
+   case "3":
+       console.log(`Week 3:${plants * 8}`);
+       break
+   default:
+       console.log("No Crops Produced this week")
+       break;
+}
